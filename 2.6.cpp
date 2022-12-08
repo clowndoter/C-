00c24125c2c59e9a4a6f8ed4184e260253ec5126
clowndoter <artem.omelchenko1337@gmail.com>
2022-12-08 23:03:40 +0200

    first

---
+++ 2.6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  long num;
+  int counter=0;
+  while (true){
+    cin >> num;
+    while (num!=1){
+      if (num%2==0){
+        num=num/2;
+      }else if (num%2!=0){
+        num=3*num+1;
+      }
+      cout << num << endl;
+      counter++;
+    }
+    cout << "Steps = ";
+    cout << counter << endl;
+  }
+}
